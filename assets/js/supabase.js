b41da493224bea3a78b8062d1478fb57ee5d769f
nifelux <globalpaysure@gmail.com>
2026-07-15 14:55:24 +0100

    Update Supabase URL and anon key

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://smxabmjoawhtyotufmgi.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNteGFibWpvYXdodHlvdHVmbWdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMTc4MjAsImV4cCI6MjA5OTY5MzgyMH0.8hYewh2Ya4hrcfKJ6qKJO6zZ8HdQ5jc1XJ6PrP_vyds";
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
